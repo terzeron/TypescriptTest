c13bab288c70e6271bdebe442a81f9f0ca278d66
Young-il Cho <youngilcho@nhnent.com>
2019-11-13 15:40:41 +0900

    add example code

--- src/test1.ts
+++ src/test1.ts
@@ -24,7 +24,73 @@ export function test1() {
     let list: number[] = [1, 2, 3];
     //let list: Array<number> = [1, 2, 3];
     console.log(list);
-   
+
+    let x: [string, number];
+    x = ["hello", 10];
+    console.log(x);
+    console.log(x[0].substr(1));
+    //console.log(x[1].substr(1));
+    //x = [10, "hello"];
+    //console.log(x);
+
+    //x[2] = "hello";
+    //x[3] = "world";
+    //console.log(x[5].toString());
+    //x[6] = true;
 }
 
+export function test2() {
+    enum Color { Red, Green, Blue};
+    let c: Color = Color.Green;
+    console.log(c);
+    let colorName: string = Color[Color.Blue];
+    console.log(colorName);
+
+    let notSure: any = 4;
+    notSure = "문자열";
+    console.log(notSure);
+    notSure = false;
+    console.log(notSure);
+    //notSure.ifItExists();
+    //notSure.toFixed();
+
+    let prettySure: Object = 4;
+    //prettySure.toFixed();
+
+    let list: any[] = [1, true, "free"]
+    list[1] = 100;
+    console.log(list);
+
+    let unusable: void = undefined;
+    let u: undefined = undefined;
+    let n: null = null;
+}
+
+function warnUser(): void {
+    console.log("This is my warning message");
+}
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+function fail(): never {
+    return error("Something failed");
+}
+
+function infiniteLoop(): never {
+    while (true) {
+    }
+}
+
+export function test3() {
+    let someValue: any = "this is a string";
+    let strLength: number = (<string>someValue).length;
+    console.log(someValue);
+    console.log(strLength);
+}
+
+
 test1();
+test2();
+test3();
